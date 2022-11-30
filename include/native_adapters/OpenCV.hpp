e881ae2ccd9e62af83d63bce4e8ec8d52ac88509
Patrick Roncagliolo <ronca.pat@gmail.com>
2022-11-30 09:51:06 +0100

    Upload package

---
+++ include/native_adapters/OpenCV.hpp
@@ -0,0 +1,85 @@
+#ifndef TRAVERSABILITY_TOOLKIT__ADAPTERS__OCV
+#define TRAVERSABILITY_TOOLKIT__ADAPTERS__OCV
+
+#include "rclcpp/type_adapter.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include <std_msgs/msg/header.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+#include <nav_msgs/msg/map_meta_data.hpp>
+#include <opencv2/core/mat.hpp>
+
+#include <signal.h>
+
+struct StampedCvMat {
+    std_msgs::msg::Header header;
+    nav_msgs::msg::MapMetaData info;
+    cv::Mat mat;
+
+    static const uint8_t UNKNOWN = 0;
+    static const uint8_t MIN_COST = 2;
+    static const uint8_t OBSTACLE = 101;
+
+    StampedCvMat() = default;
+
+    StampedCvMat(const StampedCvMat &other){      
+      std::cerr << "[OccupancyGrid Adapter] Copy constructor called" << std::endl;
+      raise(SIGTRAP);
+      this->header = other.header;
+      this->info = other.info;
+      this->mat = other.mat.clone();
+    }
+
+    StampedCvMat(StampedCvMat &&other){
+      std::cerr << "[OccupancyGrid Adapter] Move constructor called" << std::endl;
+      raise(SIGTRAP);
+      this->header = std::move(other.header);
+      this->info = std::move(other.info);
+      this->mat = std::move(other.mat);
+    }
+
+    StampedCvMat & operator=(const StampedCvMat & other){
+      std::cerr << "[OccupancyGrid Adapter] Assignment operator called" << std::endl;
+      raise(SIGTRAP);
+      if (this == &other) return *this; 
+      this->header = other.header;
+      this->info = other.info;
+      this->mat = other.mat.clone();
+    }
+};
+
+template<>
+struct rclcpp::TypeAdapter<StampedCvMat, nav_msgs::msg::OccupancyGrid>
+{
+  using is_specialized = std::true_type;
+  using custom_type = StampedCvMat;
+  using ros_message_type = nav_msgs::msg::OccupancyGrid;
+
+  static void convert_to_ros_message (const custom_type & source, ros_message_type & destination){
+    std::cerr << "[OccupancyGrid Adapter] Conversion to message" << std::endl;
+    //raise(SIGTRAP);
+
+    destination.data.resize(source.mat.rows * source.mat.cols);
+    for(int y=0; y<source.mat.rows; ++y){
+        for(int x=0; x<source.mat.cols; ++x){
+            destination.data[y*source.mat.cols+x] = source.mat.at<uint8_t>(y,x) - 1;
+        }
+    }
+    destination.header = source.header;
+    destination.info = source.info;
+  }
+
+  static void convert_to_custom (const ros_message_type & source, custom_type & destination){
+    std::cerr << "[OccupancyGrid Adapter] Conversion from message" << std::endl;
+    destination.mat = cv::Mat (source.info.height, source.info.width, CV_MAKETYPE(cv::DataType<uint8_t>::type, 1));
+    for(unsigned int y=0; y<source.info.height; ++y){
+        for(unsigned int x=0; x<source.info.width; ++x){
+            destination.mat.at<uint8_t>(y,x) = source.data[y*source.info.width+x] + 1;
+        }
+    }
+    destination.header = source.header;
+    destination.info = source.info;
+  }
+};
+
+#endif // TRAVERSABILITY_TOOLKIT__ADAPTERS__OCV
---
+++ include/native_adapters/PCL_2.hpp
@@ -0,0 +1,112 @@
+#ifndef TRAVERSABILITY_TOOLKIT__ADAPTERS__PCL_2
+#define TRAVERSABILITY_TOOLKIT__ADAPTERS__PCL_2
+
+#include "rclcpp/type_adapter.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+#include <std_msgs/msg/header.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/common.h>
+
+#include <variant>
+
+struct StampedPointCloud2 {
+    std_msgs::msg::Header header;
+    std::variant<
+      pcl::PointCloud<pcl::PointXYZ>,
+      pcl::PointCloud<pcl::PointXYZI>,
+      pcl::PointCloud<pcl::PointXYZRGBA>,
+      pcl::PointCloud<pcl::PointXYZRGB>,
+      pcl::PointCloud<pcl::PointNormal>,
+      pcl::PointCloud<pcl::PointXYZRGBNormal>,
+      pcl::PointCloud<pcl::PointXYZINormal>
+    > cloud;
+
+    StampedPointCloud2() = default;
+
+    StampedPointCloud2(const StampedPointCloud2 &other){
+      std::cerr << "[PointCloud2 Adapter] Copy constructor called" << std::endl;
+      raise(SIGTRAP);
+      this->header = other.header;
+      this->cloud = other.cloud;
+    }
+
+    StampedPointCloud2(StampedPointCloud2 &&other){
+      std::cerr << "[PointCloud2 Adapter] Move constructor called" << std::endl;
+      raise(SIGTRAP);
+      this->header = std::move(other.header);
+      this->cloud = std::move(other.cloud);
+    }
+
+    StampedPointCloud2 & operator=(const StampedPointCloud2 & other){
+      std::cerr << "[PointCloud2 Adapter] Assignment operator called" << std::endl;
+      raise(SIGTRAP);
+      if (this == &other) return *this; 
+      this->header = other.header;
+      this->cloud = other.cloud;
+    }
+
+};
+
+template<>
+struct rclcpp::TypeAdapter<StampedPointCloud2, sensor_msgs::msg::PointCloud2>
+{
+  using is_specialized = std::true_type;
+  using custom_type = StampedPointCloud2;
+  using ros_message_type = sensor_msgs::msg::PointCloud2;
+
+  static void convert_to_ros_message (const custom_type & source, ros_message_type & destination){
+    std::cerr << "[PointCloud2 Adapter] Conversion to message" << std::endl;
+    std::visit([&](auto&& cloud){pcl::toROSMsg(cloud, destination);}, source.cloud);
+    destination.header = source.header;
+  }
+
+  static void convert_to_custom (const ros_message_type & source, custom_type & destination){
+    std::cerr << "[PointCloud2 Adapter] Conversion from message" << std::endl;
+    bool rgb = false;
+    bool rgba = false;
+    bool intensity = false;
+    bool normals = false;
+    for (auto f : source.fields) {
+      if (f.name == "rgb") {
+        rgb = true;
+      }
+      if (f.name == "rgba") {
+        rgba = true;
+      }
+      if (f.name == "intensity") {
+        intensity = true;
+      }
+      if (f.name == "normal_x") {
+        normals = true;
+      }
+    }
+
+    if (rgba) {
+      process_message<pcl::PointXYZRGBA>(source, destination);
+    } else if (rgb and normals) {
+      process_message<pcl::PointXYZRGBNormal>(source, destination);
+    } else if (rgb) {
+      process_message<pcl::PointXYZRGB>(source, destination);
+    } else if (intensity and normals) {
+      process_message<pcl::PointXYZINormal>(source, destination);
+    } else if (intensity) {
+      process_message<pcl::PointXYZI>(source, destination);
+    } else if (normals) {
+      process_message<pcl::PointNormal>(source, destination);
+    } else {
+      process_message<pcl::PointXYZ>(source, destination);
+    }
+
+    destination.header = source.header;
+  }
+
+  template <typename PointT>
+  static void process_message(const ros_message_type & source, custom_type & destination){
+    destination.cloud = pcl::PointCloud<PointT>();
+    pcl::fromROSMsg(source, std::get<pcl::PointCloud<PointT>>(destination.cloud));
+  }
+};
+
+#endif // TRAVERSABILITY_TOOLKIT__ADAPTERS__PCL_2
