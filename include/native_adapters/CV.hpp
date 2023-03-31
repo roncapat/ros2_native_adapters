48a76ec5b735b0e27fc67c9093a666476e436c94
Davide Graziato <dgraziato10@gmail.com>
2023-03-31 09:36:08 +0200

    Fix general headers

--- include/native_adapters/CV.hpp
+++ include/native_adapters/CV.hpp
@@ -1,5 +1,5 @@
 #ifndef TRAVERSABILITY_TOOLKIT__ADAPTERS__CV
 #define TRAVERSABILITY_TOOLKIT__ADAPTERS__CV
-#include "native_adapters/NavMsgsOccupancyGrid_CvMat.hpp"
-#include "native_adapters/SensorMsgsImage_CvMat.hpp"
+#include "native_adapters/NavMsgOccupancyGrid_CvMat.hpp"
+#include "native_adapters/SensorMsgImage_CvMat.hpp"
 #endif // TRAVERSABILITY_TOOLKIT__ADAPTERS__CV
