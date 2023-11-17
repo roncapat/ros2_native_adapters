4ac991b9ccf1b4c6bc690d20597d25145dff3807
Simo97Fortu <simonefortuna9@gmail.com>
2023-11-17 11:18:51 +0100

    Add Dem message to CV.hpp

--- include/native_adapters/CV.hpp
+++ include/native_adapters/CV.hpp
@@ -5,5 +5,6 @@ Copyright 2023 Patrick Roncagliolo
 #ifndef NATIVE_ADAPTERS__CV_HPP_
 #define NATIVE_ADAPTERS__CV_HPP_
 #include "native_adapters/NavMsgOccupancyGrid_CvMat.hpp"
+#include "native_adapters/DigitalElevationMapMsg_CvMat.hpp"
 #include "native_adapters/SensorMsgImage_CvMat.hpp"
 #endif  // NATIVE_ADAPTERS__CV_HPP_
